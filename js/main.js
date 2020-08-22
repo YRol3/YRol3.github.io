2a62e92f733fe62f50ceadf00c13ad27fc976668
YRol3 <s20675776@gmail.com>
2020-08-22 17:10:04 +0300

    initial angular test

--- js/main.js
+++
@@ -1,8 +0,0 @@
-function init(){
-    this.translator = new Translator();
-    this.translator.setPage(document.getElementById("main"))
-}
-
-function changeTo(lang){
-    this.translator.load(lang);
-}
